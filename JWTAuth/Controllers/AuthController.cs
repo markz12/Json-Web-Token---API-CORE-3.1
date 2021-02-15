f998d4e3c87f79b96c3ef4d14e2a1669c405859a
Mark Joseph V. Abelardo <markjoseph.abelardo18@gmail.com>
2021-02-15 18:32:22 +0800

    Add project files.

---
+++ JWTAuth/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JWTAuth.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace JWTAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IConfiguration _config;
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet(nameof(FetchToken))] // Get TOKEN - MARK VANZ
+        public async Task<IActionResult> FetchToken(string email)
+        {
+            var jwt = new JwtService(_config);
+            var token = jwt.GenerateSecurityToken(email);
+            return Ok(token); // Return Token
+        }
+    }
+}
